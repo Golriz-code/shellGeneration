bdeecd6578bc6db4f4e7cf1b324804077b5c0672
ANUJ KUMAR <65164450+A0158@users.noreply.github.com>
2023-03-21 18:51:25 +0530

    Update install.sh

--- Shell Generation/install.sh
+++ Shell Generation/install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 HOME=`pwd`
-PYTHONPATH="${PYTHONPATH}:/home/golriz/projects/def-guibault/golriz/mar/lib/python3.8/site-packages"
+PYTHONPATH="${PYTHONPATH}:path of site packages of python"
 export PYTHONPATH
-# Chamfer Distance
-source /home/golriz/projects/def-guibault/golriz/mar/bin/activate
+
 cd $HOME/extension/chamfer2D
 python setup.py install --user
